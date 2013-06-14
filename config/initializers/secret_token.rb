08915f20a37bea84e26d806eb6024c06bdde29be
Lynna Kalin <lynn.kalin@gmail.com>
2013-06-14 11:35:30 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Affin::Application.config.secret_token = '1075ef2123bea5bf3a0e595205e0e3c01cc1519be888065528a761ad66337a6c37fd71f94de4c955e11ff7fa23d1272c63a2d74a0f45a6af481150e05751cb16'
